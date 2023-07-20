6391f8cadd622a62aef312167fbca50a554086f6
disavle <instadimka@gmail.com>
2023-07-20 14:32:32 +0300

    Инициализация проекта.

---
+++ Sandbox/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Sandbox
+//
+//  Created by Dima Disavle on 20.07.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+	
+	var body: some View {
+		VStack{
+			Text("Fuck SwiftUI!")
+		}
+	}
+	
+}
+
+struct ContentView_Previews: PreviewProvider {
+	static var previews: some View {
+		ContentView()
+	}
+}
+
---
+++ Sandbox/SandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandboxApp.swift
+//  Sandbox
+//
+//  Created by Dima Disavle on 20.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct SandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
