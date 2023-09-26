86b4d23d48cd02b5f00832dfb8c4f92b779af078
disavle <instadimka@gmail.com>
2023-09-26 20:05:22 +0300

    Созданы координаторы табов.

---
+++ Sandbox/Sources/Coordinators/ThemeCoordinator.swift
@@ -0,0 +1,43 @@
+//
+//  ThemeCoordinator.swift
+//  Sandbox
+//
+//  Created by Dima Disavle on 26.09.2023.
+//  Copyright © 2023 Disavle. All rights reserved.
+//
+
+import SwiftUI
+
+/// Координатор экрана темы.
+final class ThemeCoordinator: ICoordinator {
+	@Published var path: [ViewScene] = []
+	@Published var sheet: ViewScene?
+	@Published var fullScreen: ViewScene?
+	@Published var childCoordinators: [any ICoordinatorCycle] = []
+	weak var parentCoordinator: (any ICoordinatorCycle)?
+	weak var finishDelegate: CordinatorFinishDelegate?
+
+	/// Перечисление сцен.
+	enum ViewScene: Identifiable {
+		var id: UUID { return UUID() }
+		case start
+	}
+
+	/// Билдер для экрана.
+	/// - Parameter view: Экран ViewScene.
+	/// - Returns: Экран для отображения.
+	@ViewBuilder
+	func build(_ view: ViewScene) -> some View {
+		switch view {
+		case .start:
+			ThemeViewView().assembly(
+				inputModel: ThemeViewModel.InputModel(title: "Fuck SwiftUI!"),
+				outputModel: ThemeViewModel.OutputModel()
+			)
+		}
+	}
+
+	func start() {
+		path = [.start]
+	}
+}
